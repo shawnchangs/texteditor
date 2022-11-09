009e44d260761aa5c2cdd37cbc4aef5e1108c1c3
Shawn Chang <shawnchang@shawnchang-m62.lan>
2022-11-08 16:35:49 -0800

    works

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,9 +28,9 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-    ({ request }) => ["style", "script", "worker"].includes(request.destination),
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName, 'asset-cache',
+    cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
